3b7d6867c9163fd6327aad73799c0eb0143b3908
azzasalim <65505615+azzasalim@users.noreply.github.com>
2026-03-04 03:35:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -313,8 +313,8 @@ if st.session_state.role == "admin":
             data = json.loads(rj)
             rubric = data.get("rubric_scores", {})
             if isinstance(rubric, dict) and rubric:
-          scores.append(sum(rubric.values()))
-           if isinstance(rubric, dict) and rubric:
+            scores.append(sum(rubric.values()))
+            if isinstance(rubric, dict) and rubric:
             total_scores.append(sum(rubric.values()))
             total_error_density.append(data["error_density"])
 
